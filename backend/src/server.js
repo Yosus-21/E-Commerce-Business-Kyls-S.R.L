7c80a79a67c4896a92a116ea090ffe8921399f92
Yosus-21 <yosus2477@gmail.com>
2026-03-01 13:47:17 -0400

    desactivando dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 const app = require('./app');
 const { connectDB } = require('./config/database');
 const { syncModels } = require('./models/index');
